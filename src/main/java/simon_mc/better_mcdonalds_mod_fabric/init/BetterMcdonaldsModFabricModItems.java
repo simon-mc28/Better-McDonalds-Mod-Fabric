bd3dc80488a6cada52141d563841d566eabbdcf8
simon-mc28 <hartkopfmoritz5@gmail.com>
2023-02-22 20:32:48 +0100

    Added beef patty

--- src/main/java/simon_mc/better_mcdonalds_mod_fabric/init/BetterMcdonaldsModFabricModItems.java
+++ src/main/java/simon_mc/better_mcdonalds_mod_fabric/init/BetterMcdonaldsModFabricModItems.java
@@ -7,7 +7,9 @@ package simon_mc.better_mcdonalds_mod_fabric.init;
 import simon_mc.better_mcdonalds_mod_fabric.item.SaltItem;
 import simon_mc.better_mcdonalds_mod_fabric.item.MayonnaiseItem;
 import simon_mc.better_mcdonalds_mod_fabric.item.KnifeItem;
+import simon_mc.better_mcdonalds_mod_fabric.item.CookedBeefPattyItem;
 import simon_mc.better_mcdonalds_mod_fabric.item.CheeseItem;
+import simon_mc.better_mcdonalds_mod_fabric.item.BeefPattyItem;
 import simon_mc.better_mcdonalds_mod_fabric.BetterMcdonaldsModFabricMod;
 
 import net.minecraft.world.item.Item;
@@ -21,6 +23,8 @@ public class BetterMcdonaldsModFabricModItems {
 	public static Item CHEESE;
 	public static Item MAYONNAISE;
 	public static Item KNIFE;
+	public static Item BEEF_PATTY;
+	public static Item COOKED_BEEF_PATTY;
 
 	public static void load() {
 		SALT_BLOCK = Registry.register(Registry.ITEM, new ResourceLocation(BetterMcdonaldsModFabricMod.MODID, "salt_block"),
@@ -29,5 +33,7 @@ public class BetterMcdonaldsModFabricModItems {
 		CHEESE = Registry.register(Registry.ITEM, new ResourceLocation(BetterMcdonaldsModFabricMod.MODID, "cheese"), new CheeseItem());
 		MAYONNAISE = Registry.register(Registry.ITEM, new ResourceLocation(BetterMcdonaldsModFabricMod.MODID, "mayonnaise"), new MayonnaiseItem());
 		KNIFE = Registry.register(Registry.ITEM, new ResourceLocation(BetterMcdonaldsModFabricMod.MODID, "knife"), new KnifeItem());
+		BEEF_PATTY = Registry.register(Registry.ITEM, new ResourceLocation(BetterMcdonaldsModFabricMod.MODID, "beef_patty"), new BeefPattyItem());
+		COOKED_BEEF_PATTY = Registry.register(Registry.ITEM, new ResourceLocation(BetterMcdonaldsModFabricMod.MODID, "cooked_beef_patty"), new CookedBeefPattyItem());
 	}
 }
