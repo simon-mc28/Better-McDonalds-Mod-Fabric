ca4fd9cdd2addb3fb054afff117696ccbde448b7
simon-mc28 <hartkopfmoritz5@gmail.com>
2023-02-23 12:54:40 +0100

    Added sweet 'n sour sauce

--- src/main/java/simon_mc/better_mcdonalds_mod_fabric/init/BetterMcdonaldsModFabricModItems.java
+++ src/main/java/simon_mc/better_mcdonalds_mod_fabric/init/BetterMcdonaldsModFabricModItems.java
@@ -4,6 +4,7 @@
  */
 package simon_mc.better_mcdonalds_mod_fabric.init;
 
+import simon_mc.better_mcdonalds_mod_fabric.item.SweetSourSauceItem;
 import simon_mc.better_mcdonalds_mod_fabric.item.SaltItem;
 import simon_mc.better_mcdonalds_mod_fabric.item.MayonnaiseItem;
 import simon_mc.better_mcdonalds_mod_fabric.item.KnifeItem;
@@ -25,8 +26,9 @@ public class BetterMcdonaldsModFabricModItems {
 	public static Item BEEF_PATTY;
 	public static Item COOKED_BEEF_PATTY;
 	public static Item MAYONNAISE;
-	public static Item KNIFE;
+	public static Item SWEET_SOUR_SAUCE;
 	public static Item FRIES;
+	public static Item KNIFE;
 
 	public static void load() {
 		SALT_BLOCK = Registry.register(Registry.ITEM, new ResourceLocation(BetterMcdonaldsModFabricMod.MODID, "salt_block"),
@@ -36,7 +38,8 @@ public class BetterMcdonaldsModFabricModItems {
 		BEEF_PATTY = Registry.register(Registry.ITEM, new ResourceLocation(BetterMcdonaldsModFabricMod.MODID, "beef_patty"), new BeefPattyItem());
 		COOKED_BEEF_PATTY = Registry.register(Registry.ITEM, new ResourceLocation(BetterMcdonaldsModFabricMod.MODID, "cooked_beef_patty"), new CookedBeefPattyItem());
 		MAYONNAISE = Registry.register(Registry.ITEM, new ResourceLocation(BetterMcdonaldsModFabricMod.MODID, "mayonnaise"), new MayonnaiseItem());
-		KNIFE = Registry.register(Registry.ITEM, new ResourceLocation(BetterMcdonaldsModFabricMod.MODID, "knife"), new KnifeItem());
+		SWEET_SOUR_SAUCE = Registry.register(Registry.ITEM, new ResourceLocation(BetterMcdonaldsModFabricMod.MODID, "sweet_sour_sauce"), new SweetSourSauceItem());
 		FRIES = Registry.register(Registry.ITEM, new ResourceLocation(BetterMcdonaldsModFabricMod.MODID, "fries"), new FriesItem());
+		KNIFE = Registry.register(Registry.ITEM, new ResourceLocation(BetterMcdonaldsModFabricMod.MODID, "knife"), new KnifeItem());
 	}
 }
